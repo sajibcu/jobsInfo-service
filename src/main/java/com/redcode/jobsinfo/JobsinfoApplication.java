98856c512b3db7143d01fc3bb9d6a05ffd10db14
Md. Sajib Hosen <0sajib0@gmail.com>
2019-09-05 22:32:13 +0600

    initial commit

---
+++ src/main/java/com/redcode/jobsinfo/JobsinfoApplication.java
@@ -0,0 +1,14 @@
+package com.redcode.jobsinfo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobsinfoApplication {
+
+	public static void main(String[] args) {
+		
+		SpringApplication.run(JobsinfoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/redcode/jobsinfo/JobsinfoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.redcode.jobsinfo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobsinfoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
